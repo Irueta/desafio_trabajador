5077158aaeb69cb04be41d6e6f1c75c6913d2e22
Irueta <irueta@gmail.com>
2024-01-11 15:33:36 +0100

    encuestar maketa empezado

--- src/components/UserBar.jsx
+++ src/components/UserBar.jsx
@@ -3,34 +3,7 @@ import { fetchUserData, getDaytime } from "../../apiDesafio.js";
 import "./UserBar.css";
 
 const Userbar = () => {
-    const [userData, setUserData] = useState(null);
-    const [daytime, setDaytime] = useState(null);
 
-
-
-
-
-    /* useEffect(() => {
-        const fetchData = async () => {
-            const {response,data} = await fetchUserData();
-            console.log("fetchUserData",await fetchUserData());
-            if (response.status === 200) {
-                setUserData(data);
-            }
-        }
-const getTime=()=>{ 
-    const time = getDaytime();
-    setDaytime(time);
-}
-        getTime();
-        fetchData();
-    }, []);
-
-
-  if (!userData) {
-    
-    return <div>Cargando...</div>;
-    } */
 
     return (
 
--- src/pages/Perfil.jsx
+++ src/pages/Perfil.jsx
@@ -9,7 +9,92 @@ const Perfil = () => {
         return (
             <div className="perfil">
                 <UserBar />
-                <h1 className='perfilTitle'>Perfil</h1>
+                <div className='perfilPestañas'>
+                <h1 className='perfilTitle'>ENCUESTAS</h1>
+                <h1 className='perfilTitle'>DOCUMENTOS</h1>
+                <h1 className='perfilTitle'>FLOW</h1>
+                </div>
+                <div className='ultimasEncuestas'>
+                        <p className='ultimas'>Últimas encuestas</p>
+                        <p className='verMas'>VER MAS</p>
+                </div>
+                <div className='encuestas'>
+                    <div className='encuesta1'>
+                        <div className='imagenEncuesta'>
+                        <img src="/encuestas/imagen.svg" alt="" />
+                        </div>
+                        <div className='infoEncuesta'>
+                            <div className='info1'>
+                                <p className='encuestaTitulo'>TEMATICA</p>
+                                <p className='encuestaTexto'>Compañerismo en el trabajo</p>
+                            </div>
+                            <div className='info2'>
+                                <div className='info2_1'>
+                                <p className='encuestaTitulo'>FECHA DE CREACIÓN</p>
+                                <p className='encuestaTexto'>09/01/2024</p>
+                                </div>
+                                <div className='info2_2'>
+                                <button className='participar'>PARTICIPAR</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='encuesta2'>
+                        <div className='imagenEncuesta'>
+                        <img src="/encuestas/imagen.svg" alt="" />
+                        </div>
+                        <div className='infoEncuesta'>
+                            <div className='info1'>
+                                <p className='encuestaTitulo'>TEMATICA</p>
+                                <p className='encuestaTexto'>Compañerismo en el trabajo</p>
+                            </div>
+                            <div className='info2'>
+                                <div className='info2_1'>
+                                <p className='encuestaTitulo'>FECHA DE CREACIÓN</p>
+                                <p className='encuestaTexto'>09/01/2024</p>
+                                </div>
+                                <div className='info2_2'>
+                                <button className='participar'>PARTICIPAR</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='encuesta3'>
+                        <div className='imagenEncuesta'>
+                        <img src="/encuestas/imagen.svg" alt="" />
+                        </div>
+                        <div className='infoEncuesta'>
+                            <div className='info1'>
+                                <p className='encuestaTitulo'>TEMATICA</p>
+                                <p className='encuestaTexto'>Compañerismo en el trabajo</p>
+                            </div>
+                            <div className='info2'>
+                                <div className='info2_1'>
+                                <p className='encuestaTitulo'>FECHA DE CREACIÓN</p>
+                                <p className='encuestaTexto'>09/01/2024</p>
+                                </div>
+                                <div className='info2_2'>
+                                <button className='participar'>PARTICIPAR</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className='ultimasEncuestas'>
+                        <p className='ultimas'>Encuestas realizadas</p>
+                        <p className='verMas'>VER MAS</p>
+                </div>
+                <div className='encuestasRealizadas'>
+                    <div className='realizada1'>
+                        <div className='realizadaTitulo'>SOSTENIBILIDAD</div>
+                        <div className='realizadaImgBut'>
+                            <img src="/encuestas/imagenRealizada1.svg" alt="" />
+                            <button className='ver'>VER</button>
+                        </div>
+
+                    </div>
+                </div>
+
                 <NavBar />
             </div>
         )
